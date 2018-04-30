733cecc834b098f2f2bcf5baaa62b8f5b9490f08
Tim Wilson <timwilsonr@gmail.com>
2018-04-30 16:00:13 -0700

    test 2

---
+++ code/tim/lab03_grades.py
@@ -0,0 +1,38 @@
+while True:
+    try:
+        answer = float(input('What did you get on the test? > '))
+        break
+    except ValueError:
+        print('Enter as Number')
+        continue
+
+if answer > 100:
+    mod_grade = 9
+else:
+    mod_grade = answer % 10
+
+if answer >= 90:
+    answer = 'A'
+    congrats = 'Good Job!'
+elif answer >= 80:
+    answer = 'B'
+    congrats = 'Good Job!'
+elif answer >= 70:
+    answer = 'C'
+    congrats = ''
+elif answer >= 60:
+    answer = 'D'
+    congrats = 'Booooo!'
+else:
+    answer = 'F'
+    congrats = 'Booooo!'
+    mod_grade = 5
+
+if mod_grade >= 7:
+    mod_grade = '+'
+elif mod_grade <= 3:
+    mod_grade = '-'
+else:
+    mod_grade = ''
+
+print('You got a', answer  + mod_grade + '.', congrats)
