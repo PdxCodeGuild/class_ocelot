196f9089f99149503fa21edc5b691600a8d17b8f
Shane Cheek <unitedideas@gmail.com>
2018-04-30 14:04:07 -0700

    initial commit

---
+++ code/Shane/lab01turt.py
@@ -0,0 +1,45 @@
+from turtle import *
+
+penup()
+
+goto(75,180)
+
+pendown()
+
+for i in range(180):
+    forward (1)
+    left(2)
+
+setheading(270)
+
+forward(15)
+
+setheading(0)
+
+forward(50)
+
+backward(50)
+
+setheading(180)
+
+forward(50)
+
+backward(50)
+
+setheading(270)
+
+forward (90)
+
+right(45)
+
+forward (90)
+
+backward(90)
+
+setheading(270)
+
+left(45)
+
+forward (90)
+
+backward(90)
