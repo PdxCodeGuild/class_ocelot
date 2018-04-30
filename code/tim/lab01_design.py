72582783be4d73e0b68288f025ea4d205b98995b
Tim Wilson <timwilsonr@gmail.com>
2018-04-30 13:43:34 -0700

    test 2

--- code/tim/lab01_design.py
+++ code/tim/lab01_design.py
@@ -6,16 +6,17 @@ grow = 4
 edge_length = 20
 i = 0
 
-lColors = ['Red', 'Green', 'Blue', 'Yellow', 'purple', 'Orange']
+lColors = ['Red', 'Yellow', 'Green', 'Blue', 'purple', 'Orange']
 
 while i < 200:
     forward(edge_length)
     right(170)
     edge_length = edge_length + grow
     if (i % 20) == 0:
-        color(acolor)
+        color(lColors[acolor])
         acolor += 1
-        grow *= -1
+        if acolor >= len(lColors):
+            acolor = 0
     i = i + 1
 
 left(90)
