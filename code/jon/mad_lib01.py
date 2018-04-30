517bb91c6f002dcdb08c9cfc5aff0f1e35cdd2be
jbook <jbook85@gmail.com>
2018-04-30 15:10:07 -0700

    mad_lib01

--- code/jon/mad_lib01.py
+++ code/jon/mad_lib01.py
@@ -1,7 +1,12 @@
-a1 = input('verb')
+a = input('verb')
 b = input('plural body part')
 c = input('body sense')
-print(f"""The day i saw the monkey king {a1}.
+d = input('past tense verb')
+e = input('fruit')
+g = input ('amount of time')
+
+print(f"""The day i saw the monkey king {a}.
 It was the most interesting day of the year.
 I still cannot believe my {b}. 
-I had {c} running through my body for a week.""")
+I had {c} running through my body for a week.
+The monkey king {d} one of the largest {e} I had seen in {g}.""")
