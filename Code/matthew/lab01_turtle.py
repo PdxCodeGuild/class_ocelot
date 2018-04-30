92b7ee45812957196fc64965b148997e640baca2
flux2341 <flux2341@gmail.com>
2018-04-30 15:09:02 -0700

    random stuff

---
+++ Code/matthew/lab01_turtle.py
@@ -0,0 +1,25 @@
+from turtle import *
+
+
+
+colors = ['red', 'green', 'blue']
+
+
+# i = 0
+# while i < 100:
+#     print(i, i%2)
+#     i += 1
+
+
+#
+# n_sides = 100
+# i = 0
+#
+# while i < n_sides:
+#     color(colors[i%len(colors)])
+#     print(i, i%len(colors))
+#     forward(2)
+#     left(360/n_sides)
+#     i = i + 1
+#
+# done()
