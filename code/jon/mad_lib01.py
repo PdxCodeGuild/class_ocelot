a5b101f031ffc3d0a44cbb44b1140b0bcd68f703
jbook <jbook85@gmail.com>
2018-04-30 14:58:00 -0700

    mad lib_01

--- code/jon/mad_lib01.py
+++ code/jon/mad_lib01.py
@@ -2,6 +2,6 @@ a1 = input('verb')
 b = input('plural body part')
 c = input('body sense')
 print(f"""The day i saw the monkey king {a1}.
-      It was the most interesting day of the year.
-    I still cannot believe my {b}. 
-    I had {c} running through my body for a week.""")
+It was the most interesting day of the year.
+I still cannot believe my {b}. 
+I had {c} running through my body for a week.""")
