35bcffa5a8a2afcb5c097b420afd1faa17dd11c5
Shane Cheek <unitedideas@gmail.com>
2018-05-01 11:37:47 -0700

    v4 asking to play again w/ help from Matthew

--- Code/Shane/lab04_magic_8_ball.py
+++ Code/Shane/lab04_magic_8_ball.py
@@ -23,19 +23,18 @@ out_list = ['It is certain',
 'Outlook not so good',
 'Very doubtful']
 
+
 while True:
 
     input('...Ask any yes or no question...\n')
 
     print(random.choice(out_list))
 
-    play = input('Would you like to play again? yes or no\n')
-
+    while True:
+        play = input('Would you like to play again? yes or no\n')
+        if play == 'yes' or play == 'no':
+            break
     if play == 'no':
         break
-
-    #while play != 'yes' and play != 'no':
-        # play = input('Would you like to play again? yes or no\n')
-
 
 
