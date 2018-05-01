aea7e471f5e9f0040bb032652d63862293e5cca6
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-01 13:37:51 -0700

    While loop added to generate 5 faces.

--- Code/Steven/lab05_random_emoticon1.py
+++ Code/Steven/lab05_random_emoticon1.py
@@ -6,16 +6,13 @@ beard_state = random.randint(1, 4)
 
 
 # define emoticon face components
-emo_eyeses = [':', ';', '|', '8']
+emo_eyeses = [':', ';', '8']
 emo_noses = ['-', 'o', '{']
 emo_mouths = [')', '|', '(', 'o', '/', 'P', 'D']
 emo_beards = ['>', '>']
 
 
-# randomly select face components
-emo_eyes = random.choice(emo_eyeses)
-emo_nose = random.choice(emo_noses)
-emo_mouth = random.choice(emo_mouths)
+
 
 if beard_state == 1:
     emo_beard = random.choice(emo_beards)
@@ -24,4 +21,11 @@ else:
     emo_beard = ''
 
 # display emoticon
-print(emo_eyes+emo_nose+emo_mouth+emo_beard)
+counter1 = 0
+while counter1 < 5:
+    # randomly select face components
+    emo_eyes = random.choice(emo_eyeses)
+    emo_nose = random.choice(emo_noses)
+    emo_mouth = random.choice(emo_mouths)
+    print(emo_eyes + emo_nose + emo_mouth + emo_beard)
+    counter1 += 1
