974a92330ad7a173cfdf5d8016f03a88e4ea9ef9
Tim Wilson <timwilsonr@gmail.com>
2018-05-01 16:14:48 -0700

    lab03 - final commit

---
+++ Code/tim/lab07_RPS.py
@@ -0,0 +1,26 @@
+import random
+import chalk
+
+rps = ['ROCK', 'PAPER', 'SCISSORS']
+
+h_ans = input('Rock, Paper or Scissors? > ').upper()
+c_ans = random.choice(rps)
+print(f'Computer picks: ' + chalk.red(c_ans))
+
+if h_ans in rps:
+    if h_ans == c_ans:
+        print('tie, lame')
+    elif h_ans == 'ROCK' and c_ans == 'PAPER':
+        print('Paper covers rock, you lose')
+    elif c_ans == 'ROCK' and h_ans == 'PAPER':
+        print('Paper covers rock, you win')
+    elif c_ans == 'ROCK' and h_ans == 'SCISSORS':
+        print('Rock smashes scissors, you lose')
+    elif h_ans == 'ROCK' and c_ans == 'SCISSORS':
+        print('Rock smashes scissors, you win')
+    elif c_ans == 'SCISSORS' and h_ans == 'PAPER':
+        print('Scissors cuts paper, you lose')
+    else:
+        print('Scissors cuts paper, you win')
+else:
+    print(f'{c_ans} beats {h_ans} because you didn\'t follow the instructions, ya hoser')
