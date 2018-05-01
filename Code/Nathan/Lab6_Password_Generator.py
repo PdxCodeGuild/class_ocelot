3068df896fe47d13e215a60ab86198a6ac0dbee0
Nathan Kinney <nathankeithkinney@gmail.com>
2018-05-01 15:11:59 -0700

    v2-shuffle

--- Code/Nathan/Lab6_Password_Generator.py
+++ Code/Nathan/Lab6_Password_Generator.py
@@ -4,11 +4,14 @@ import random
 user_response_letters = int(input('How many letters would you like your password to be?\n'))
 user_response_digits = int(input('How many digits would you like in the password?\n '))
 
-password= ''
+password= []
 
 for i in range(user_response_letters):
     password += (random.choice(string.ascii_letters))
-    print(password)
+
 for i in range(user_response_digits):
     password += (random.choice(string.digits))
-    print(password)
+random.shuffle(password)
+password= ''.join(password)
+print(password)
+
