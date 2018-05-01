fb8626c7b3c8c5c6b2a7ad0d0117453e54956996
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-01 16:19:59 -0700

    initial try.

---
+++ Code/Steven/lab07_RPScissors1.py
@@ -0,0 +1,40 @@
+import string
+import random
+
+# Computer select Rock, Paper or Scissors.
+RPS_choices = ['Rock', 'Paper', 'Scissors']
+
+
+computer_selection = random.choice(RPS_choices)
+
+
+# User select Rock, Paper or Scissors.
+user_input = int(input('Enter 1 for Rock, 2 for Paper, or 3 for Scissors: '))
+user_selection = RPS_choices[user_input -1]
+
+# Determine Winner
+if user_selection == computer_selection:
+    play_result = 'Tie'
+elif user_selection == 'Rock' and computer_selection == 'Paper':
+    play_result = "Lose"
+elif user_selection == 'Rock' and computer_selection == 'Scissors':
+    play_result = "Win"
+elif user_selection == 'Paper' and computer_selection == 'Scissors':
+    play_result = "Lose"
+elif user_selection == 'Paper' and computer_selection == 'Rock':
+    play_result = "Win"
+elif user_selection == 'Scissors' and computer_selection == 'Rock':
+    play_result = "Lose"
+elif user_selection == 'Scissors' and computer_selection == 'Paper':
+    play_result = "Win"
+
+
+# Display Results
+print(user_selection)
+print(computer_selection)
+print(play_result)
+
+
+
+# Keep Running tally of score.
+
