92fcbcda623b91ce4f116e6b71d64205cf2784f5
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-02 12:10:20 -0700

    Initial Guess the Number, v01

---
+++ Code/Steven/lab12_Guess_Number1.py
@@ -0,0 +1,12 @@
+import random
+
+
+print('\nYour classroom is set to self-destruct in 15 seconds! - can you guess the deactivation code?\n\t(It is an integer between 1 and 10)')
+
+user_guess = input('\tENTER DEACTIVATION CODE >')
+
+
+x = random.randint(1,10)
+print(x)
+
+
