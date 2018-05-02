958b13c6a8b40dd0bcde29fb8b9b2f9e1e29db79
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-02 10:12:57 -0700

    Removed unnecessary 'int()' commands.

--- Code/Steven/lab08_make_change1.py
+++ Code/Steven/lab08_make_change1.py
@@ -11,19 +11,19 @@ input_amount = float(input('Enter total dollar amount and press \'Return\' to se
 print()
 
 # use as many quarters as possible
-quarter_count_FL = int(input_amount // coin_quarter)
+quarter_count_FL = input_amount // coin_quarter
 quarter_count_MD = round(input_amount % coin_quarter, 3)
 
 # use as many dimes as possible
-dime_count_FL = int(quarter_count_MD // coin_dime)
+dime_count_FL = quarter_count_MD // coin_dime
 dime_count_MD = round(quarter_count_MD % coin_dime, 3)
 
 # use as many nickels as possible
-nickel_count_FL = int(dime_count_MD // coin_nickel)
+nickel_count_FL = dime_count_MD // coin_nickel
 nickel_count_MD = round(dime_count_MD % coin_nickel, 3)
 
 # use pennies as final remainder
-penny_count_FL = int(nickel_count_MD // coin_penny)
+penny_count_FL = nickel_count_MD // coin_penny
 
 print('The minimum number of coins is', quarter_count_FL + dime_count_FL + nickel_count_FL + penny_count_FL, ".")
 print(quarter_count_FL, 'quarters,', dime_count_FL, 'dimes,', nickel_count_FL, 'nickels and', penny_count_FL, 'pennies.')
