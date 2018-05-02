6c1008f9ef2b65397d0e7e10fd9084d801be61bf
Shane Cheek <unitedideas@gmail.com>
2018-05-02 14:46:07 -0700

    init version

--- Code/Shane/lab12_guessTheNumber.py
+++ Code/Shane/lab12_guessTheNumber.py
@@ -10,9 +10,10 @@ def hot_cold(first, second):
         return ""
 
 
-guess_amount = int(input("How many chances do you get?\n:"))
+# lets the owner select the amount of guesses
+guess_limit = 2
 
-how_big = int(input("How big is this guessing game?\n:"))
+how_big = int(input("How big is this guessing game? 1 - ?\n:"))
 
 answer = random.choice(range(1, how_big+1))
 
@@ -31,28 +32,37 @@ while True:
 
     hot_or_cold.append(abs(guess - answer))
 
-    # print(guess_lst)
-    # sanity check
-    # print(hot_or_cold)
-
     if answer == guess:
         print(f"You win, the answer was {answer}.\nYou answered in {len(guess_lst)} tries\n"
               f"Your guesses were {guess_lst}")
         if input("N to stop... Enter to play again.\n").upper() == "N":
             break
         else:
-            guess_amount = int(input("How many chances do you get?\n:"))
 
-            how_big = int(input("How big is this guessing game?\n:"))
+            how_big = int(input("How big is this guessing game? 1 - ?\n:"))
 
             answer = random.choice(range(1, how_big + 1))
 
-            # here for sanity check
-            print(answer)
-
+            # resets the lists
             guess_lst = []
 
             hot_or_cold = []
+
+    elif len(guess_lst) == guess_limit:
+            print(f"You lose, the answer was {answer}.\nYou had {len(guess_lst)} tries\n"
+                  f"Your guesses were {guess_lst}")
+            if input("N to stop... Enter to play again.\n").upper() == "N":
+                break
+            else:
+
+                how_big = int(input("How big is this guessing game? 1 - ?\n:"))
+
+                answer = random.choice(range(1, how_big + 1))
+
+                # resets the lists
+                guess_lst = []
+
+                hot_or_cold = []
 
     elif guess < answer:
         print("Low")
@@ -62,3 +72,4 @@ while True:
         print("High")
         if len(guess_lst) > 1:
             print(hot_cold(hot_or_cold[-1], hot_or_cold[-2]))
+
