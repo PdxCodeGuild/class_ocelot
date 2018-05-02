8a6fd39865731e8bd20ca0801c526351c27adad3
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-02 12:28:46 -0700

    Initial Guess the Number, v02

--- Code/Steven/lab12_Guess_Number1.py
+++ Code/Steven/lab12_Guess_Number1.py
@@ -1,12 +1,21 @@
 import random
+user_guess_count = 0
+user_guess = ""
+
+# Computer sets target number
+num_target = random.randint(1,10)
+
+# print('\nYour classroom is set to self-destruct in 15 seconds! - can you guess the deactivation code?\n\t(It is an integer between 1 and 10)')
+
+print(num_target)
+
+while user_guess != num_target:
+    user_guess = int(input('\tENTER DEACTIVATION CODE >'))
+    user_guess_count = user_guess_count + 1
+    print(f'{10-user_guess_count} attempts remaining...')
 
 
-print('\nYour classroom is set to self-destruct in 15 seconds! - can you guess the deactivation code?\n\t(It is an integer between 1 and 10)')
+# Indicate result
 
-user_guess = input('\tENTER DEACTIVATION CODE >')
-
-
-x = random.randint(1,10)
-print(x)
-
+print(f'You guessed the number in {user_guess_count} tries!')
 
