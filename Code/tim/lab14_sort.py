31d6e7f731647c0b34382b963ae1dc09c008c17e
Tim Wilson <timwilsonr@gmail.com>
2018-05-03 13:40:24 -0700

    lab07 - initial review

--- Code/tim/lab14_sort.py
+++ Code/tim/lab14_sort.py
@@ -3,7 +3,7 @@ import random
 
 def random_list(n):
     lst = []
-    for i in range(n - 1):
+    for i in range(n):
         lst.append(random.randrange(100))
     return lst
 
@@ -18,7 +18,6 @@ def shuffle(lst):
 
 def is_sorted(lst):
     for i in range(1, len(lst)):
-        print(lst[i - 1], lst[i])
         if lst[i - 1] > lst[i]:
             return False
     return True
@@ -32,22 +31,30 @@ def accuracy(lst):
     return y / len(lst)
 
 
-lst = random_list(20)
+lst = random_list(14)
 i = 0
 b_acc = 0
 b_lst = lst.copy()
 while not is_sorted(b_lst):
-    lst = b_lst.copy()
-    r1 = random.randrange(len(b_lst))
-    r2 = random.randrange(len(b_lst))
+    # make temp list 'lst' and swap 2 random places
+    t_lst = b_lst.copy()
+    r1 = random.randrange(len(t_lst))
+    r2 = random.randrange(len(t_lst))
 
-    t = lst[r1]
-    lst[r1] = lst[r2]
-    lst[r2] = t
+    t_lst[r1] = b_lst[r2]
+    t_lst[r2] = b_lst[r1]
 
-    t_acc = accuracy(lst)
-    if t_acc > b_acc:
-        b_lst = lst.copy()
+    print(t_lst)
+    print(b_lst)
+
+    # check accuracy of new temp list
+    t_acc = accuracy(t_lst)
+    print(r1, r2, t_acc)
+
+    if t_acc >= b_acc:
+        b_acc = t_acc
+        b_lst = t_lst.copy()
+
     i += 1
 
 print(f'That took {i} tries')
