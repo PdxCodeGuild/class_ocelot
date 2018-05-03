c67570ac0af4de52ccabe3623dfa51d408fa126b
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-02 18:18:55 -0700

    dev 05

--- Code/Steven/lab13_ROT13-1.py
+++ Code/Steven/lab13_ROT13-1.py
@@ -1,15 +1,29 @@
+text_in = ''
+
 # Select encoding rotation (1-25).
 
 import string
 
+
 # Enter text to encode.
-text_in = input('Enter text to encode. \n>')
 
-for x in range(len(text_in)):
-    text_in_num = int(ord(text_in[x]))
-    text_in_num = text_in_num + 13
-    print(chr(text_in_num), end="")
+while text_in != '.':
+    encoding = int(input('\nSelect encoding rotation (1-25) >'))
+    text_in = input('Enter text to encode. \n>')
 
+
+    for x in range(len(text_in)):
+        text_in_num = int(ord(text_in[x]))
+
+        # Adds the rotation value to the ascii num
+        text_in_num = text_in_num + encoding
+
+        # Cycles text_nums > 26 to # 1, etc.
+        if text_in_num > 26:
+            text_in_num -= 26
+
+        # Prints entire output string to single line.
+        print(chr(text_in_num), end="")
 
 # convert to number
 
