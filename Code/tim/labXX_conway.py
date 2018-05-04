9bca9973db21fb356ac070afe760f53d2df11a64
Tim Wilson <timwilsonr@gmail.com>
2018-05-03 20:45:47 -0700

    lab14 - initial review

--- Code/tim/labXX_conway.py
+++ Code/tim/labXX_conway.py
@@ -1,20 +1,69 @@
 import random
 import chalk
+import time
 
-height = 30
+height = 20
 width = 100
 
+r_sq = chalk.red('■')
 
 def print_grid(grid):
     p_grid = ''
     for i in range(height):
         for j in range(width):
-            if grid[i][j]:
-                p_grid += chalk.red('■')
+            if grid[i][j] == '':
+                p_grid += r_sq
+                grid[i][j] = True
+            elif grid[i][j]:
+                p_grid += '■'
             else:
                 p_grid += '□'
         p_grid += '\n'
     print(p_grid)
+
+
+def draw_rand_sq(grid):
+    i = random.randrange(len(grid) - 4)
+    j = random.randrange(len(grid[1]) - 4)
+    #Top
+    grid[i][j] = ''
+    grid[i][j + 1] = ''
+    grid[i][j + 2] = ''
+    grid[i][j + 3] = ''
+    grid[i][j + 4] = ''
+    #Sides
+    grid[i + 1][j] = ''
+    grid[i + 1][j + 4] = ''
+    grid[i + 2][j] = ''
+    grid[i + 2][j + 4] = ''
+    grid[i + 3][j] = ''
+    grid[i + 3][j + 4] = ''
+    #Bottom
+    grid[i + 4][j] = ''
+    grid[i + 4][j + 1] = ''
+    grid[i + 4][j + 2] = ''
+    grid[i + 4][j + 3] = ''
+    grid[i + 4][j + 4] = ''
+    #Insides
+    grid[i + 1][j + 1] = ''
+    grid[i + 1][j + 3] = ''
+    grid[i + 2][j + 1] = ''
+    grid[i + 2][j + 3] = ''
+    grid[i + 3][j + 1] = ''
+    grid[i + 3][j + 3] = ''
+    grid[i + 1][j + 2] = ''
+    grid[i + 3][j + 2] = ''
+
+def draw_rand_streak(grid):
+    vh = random.randrange(2)
+    if vh == 0:
+        v = random.randrange(len(grid))
+        for j in range(len(grid[1])):
+            grid[v][j] = ''
+    else:
+        h = random.randrange(len(grid[1]))
+        for i in range(len(grid)):
+            grid[i][h] = ''
 
 
 def alive(grid, i, j):
@@ -73,17 +122,18 @@ grid = []
 for i in range(height):
     grid.append([])
     for j in range(width):
-        grid[i].append(random.choice([True, False]))
-# grid[height // 2 + 0][width // 2 + 0] = True
-# grid[height // 2 + 0][width // 2 + 1] = True
-# grid[height // 2 + 1][width // 2 + 0] = True
-# grid[height // 2 + 1][width // 2 + 1] = True
-
+        grid[i].append(False)
+draw_rand_sq(grid)
+draw_rand_sq(grid)
+draw_rand_streak(grid)
+draw_rand_streak(grid)
+print_grid(grid)
+time.sleep(2)
 
 n_alive = 1
+r_ct = 1
+
 while n_alive != 0:
-    print_grid(grid)
-    print('')
     n_alive = 0
     t_grid = []
     for i in range(height):
@@ -92,5 +142,12 @@ while n_alive != 0:
             t_grid[i].append(alive(grid, i, j))
             n_alive += t_grid[i][j]
     grid = deep_copy(t_grid)
-
-print_grid(grid)
+    r_ct += 1
+    if r_ct == 25:
+        draw_rand_sq(grid)
+    elif r_ct == 50:
+        r_ct = 0
+        draw_rand_streak(grid)
+    print_grid(grid)
+    print('')
+    time.sleep(.3)
