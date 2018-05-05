e552272db2369fc28c624f91ca8ea6db4edb8ba2
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-04 20:40:59 -0700

    output field mods.

--- Code/Steven/labs COMPLETED/lab04_Magic_8-ball.py
+++ Code/Steven/labs COMPLETED/lab04_Magic_8-ball.py
@@ -4,7 +4,7 @@ import random
 
 print('Welcome to the Magic 8-Ball!')
 
-confidence = int(input('How confident are you today? 2 = Very, 1 = Slightly, or 0 = Not at all. '))
+confidence = int(input('How confident are you today? 2 = Very, 1 = Slightly, or 0 = Not at all.\n>'))
 
 response_pos = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes']
 
@@ -12,9 +12,10 @@ response_neutral = ['Reply hazy try again', 'Ask again later', 'Better not tell 
 
 response_neg = ['Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
 
-the_question = input('Enter your Yes or No question. (Enter \'done\' if done) ')
 
-while the_question != 'done':
+the_question = input('\nEnter your Yes or No question. (Hit \'Enter\' when done) ')
+
+while the_question != '':
     if confidence == 2:
         response_list = random.choice([response_pos, response_neutral, response_neg, response_pos])
     else:
@@ -22,6 +23,6 @@ while the_question != 'done':
 
     magic_answer = random.choice(response_list)
 
-    print(magic_answer)
+    print(magic_answer + '\n')
     the_question = input('Enter your Yes or No question. (Enter \'done\' if done) ')
 
