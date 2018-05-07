9a0417eb947e445b3913a53c7bb005a0d92f8c8d
Tim Wilson <timwilsonr@gmail.com>
2018-05-07 16:02:07 -0700

    lab17 - Roman Numeral Converter

--- Code/tim/lab17_number_converter2.py
+++ Code/tim/lab17_number_converter2.py
@@ -1,8 +1,8 @@
 
 numerals = (('M', 1000), ('D', 500), ('C', 100), ('L', 50), ('X', 10), ('V', 5), ('I', 1))
 
-while True:
-    num = int(input('What number do you want to translate into Roman Numerals?\n > '))
+
+def to_roman_numerals(num):
     rom = ''
     for i in range(len(numerals)):
         t = num // numerals[i][1]
@@ -10,9 +10,17 @@ while True:
             rom += numerals[i][0] * t
             num -= t * numerals[i][1]
         for j in range(i + 1, len(numerals)):
-            print(num, numerals[i][1] - numerals[j][1])
-            if num > (numerals[i][1] - numerals[j][1]) and num // (numerals[i][1] - numerals[j][1]) == 1:
+            if j == i + 1 and numerals[i][1] == numerals[j][1] * 2:
+                continue
+            if num >= (numerals[i][1] - numerals[j][1]):
                 rom += numerals[j][0] + numerals[i][0]
                 num -= numerals[i][1] - numerals[j][1]
                 break
-    print(rom)
+    return rom
+
+
+for i in range(10):
+    p_table = []
+    for j in range(10):
+        p_table.append(to_roman_numerals(i * 1 + j + 1))
+    print(p_table)
