3ac7a0a9756857fd0cd88d87097b11aac22f054b
Nathan Kinney <nathankeithkinney@gmail.com>
2018-05-08 16:05:56 -0700

    v1 for fun

--- 1 Python/solutions/lab20-count_words.py
+++ 1 Python/solutions/lab20-count_words.py
@@ -1,6 +1,6 @@
 
 
-with open('the_clue_of_the_golden_coin.txt', 'r', encoding='utf-8') as f:
+with open('goldencoin.txt', 'r', encoding='utf-8') as f:
     contents = f.read()
 
 lines = contents.split('\n')
