f425af6f0a64e157644c7fceae81e646efadc3b8
Shane Cheek <unitedideas@gmail.com>
2018-05-08 16:47:12 -0700

    Book for lab 20

--- Code/Shane/lab20_count_words.py
+++ Code/Shane/lab20_count_words.py
@@ -1,3 +1,22 @@
 import string
 
-open()
+words = {}
+
+with open('the_battle_of_talavera.txt', 'r', encoding='utf-8') as f:
+    contents = f.read()
+
+for p in string.punctuation:
+    contents = contents.replace(p, ' ')
+contents = contents.replace("\n", ' ')
+
+contents = contents.lower()
+
+lines = contents.split()
+
+print(lines)
+
+for word in lines:
+    if word in words
+        words.update({'item3': 3})
+        if 'item3' in words:
+            print('I\'m here')
