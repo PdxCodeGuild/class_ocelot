57a36f7e80222b4458aa6f319f2876c66c7e7bea
Shane Cheek <unitedideas@gmail.com>
2018-05-07 23:07:02 -0700

    Final Rev.

--- Code/Shane/lab17_number_to_phrase.py
+++ Code/Shane/lab17_number_to_phrase.py
@@ -1,14 +1,13 @@
+"""
+I do not cover edge cases like 2twenty thousand,
+it will say twoty thousand, but I do cover the lower numbers
+like twenty nine etc.
+"""
+
 
 def z_to_19_dict(lst):
-    the_num = 0
-    if lst[1] == 1:
-        for i in range(10):
-            if i == lst[0]:
-                the_num = int("1" + str(i))
-                return the_num
-    else:
-        return lst[0]
-
+    if lst[0] == 1:
+        return 10+lst[1]
 
 
 z_19 = {   0: '',
@@ -30,74 +29,44 @@ z_19 = {   0: '',
            16: 'sixteen',
            17: 'seventeen',
            18: 'eighteen',
-           19: 'nineteen',}
-twenty_ = {
-           2: 'twenty',
-           3: 'thirty',
-           4: 'forty',
-           5: 'fifty',
-           6: 'sixty',
-           7: 'seventy',
-           8: 'eighty',
-           9: 'ninety',}
-hundreds= {
-           1: 'hundred',}
-thousand= {
-           1: 'thousand'
-           }
+           19: 'nineteen',
+           20: 'twenty',
+           30: 'thirty',
+           40: 'forty',
+           50: 'fifty',
+           60: 'sixty',
+           70: 'seventy',
+           80: 'eighty',
+           90: 'ninety', }
 
+x = 11
 
+remain = x
 
-
-x = "57"
-
-num_lst = list(x)
-num_lst.reverse()
-int_lst = list(map(int,num_lst))
-
-one_t0_19 = z_to_19_dict(int_lst)
-
-print(z_19[one_t0_19])
-
-output = []
-if len(int_lst) == 3:
-    output.append(z_19[int_lst[2]])
-    output.append(hundreds[1])
-if :
-    output.append(z_19[z_to_19_dict(int_lst)])
-
-print(output)
-#
-# ones = (num_lst[1]+num_lst[0])
-#
-# print(ones)
-#
-# ones = int(ones)
-#
-# print(type(ones))
-#
-# print()
-#
-# x_int = int(x)
-#
-# for i in range(len(num_lst)):
-#     tens = ""
-#     hundreds = ""
-#     print(i)
-#     print()
-#     if i == 1 or num_lst[1] == 1:
-#         print(ten_19[i-1])
-#
-# for x in range(len(x)):
-#     if x == 0:
-#         break_lst.append(x_int % 10)
-#         print(break_lst)
-#     if x == 1:
-#         break_lst.append(x_int % 100// 10)
-#         print(break_lst)
-#     if x == 2:
-#         break_lst.append(x_int % 1000 //100)
-#         print(break_lst)
-#     if x == 3:
-#         break_lst.append(x_int % 10000)
-#         print(break_lst)
+if x >= 100000:
+    hundred_thousand = (remain // 100000)
+    remain = x % 100000
+    print(f'{z_19[hundred_thousand]} hundred', end =" ")
+if x >= 10000:
+    ten_thousand = (remain // 10000)
+    remain = x % 10000
+    print(f'{z_19[ten_thousand]}ty', end=" ")
+if x >= 1000:
+    thousand = (remain // 1000)
+    remain = x % 1000
+    print(f'{z_19[thousand]} thousand', end=" ")
+if x >= 100:
+    hundred = (remain // 100)
+    remain = x % 100
+    print(f'{z_19[hundred]} hundred and', end=" ")
+if x >= 10:
+    if remain < 20:
+        lst = list(str(remain))
+        lst = list(map(int,lst))
+        print(f'{z_19[z_to_19_dict(lst)]}')
+    else:
+        lst = list(str(remain))
+        lst = list(map(int,lst))
+        print(f'{z_19[lst[0]*10]} {z_19[lst[-1]]}')
+if x < 10:
+    print(z_19[x])
