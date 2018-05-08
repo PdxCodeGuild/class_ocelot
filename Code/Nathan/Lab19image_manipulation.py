cf471092f1f5c0996f599294ddf22a4c0d75691d
Nathan Kinney <nathankeithkinney@gmail.com>
2018-05-08 14:50:36 -0700

    v1 for fun

--- Code/Nathan/Lab19image_manipulation.py
+++ Code/Nathan/Lab19image_manipulation.py
@@ -1,15 +1,22 @@
-from PIL import Image
+
+from PIL import Image, ImageDraw, ImageFont
 
 img = Image.open("lenna.png") # must be in same folder
 width, height = img.size
 pixels = img.load()
 
-for i in range(width):
-    for j in range(height):
-        r, g, b = pixels[i, j]
+draw = ImageDraw.Draw(img)
+color = (256, 128, 228)
+draw.line((0, 0, width, height), fill=color)
+draw.line((100, width, height,0), fill=color)
+
+
+# for i in range(width):
+#     for j in range(height):
+#         r, g, b = pixels[i, j]
 
         # your code here
 
-        pixels[i, j] = (r, g, b)
+     #   pixels[i, j] = (r, g, b)
 
 img.show()
