cda361a4724cc0be7a5ccbd86208725f38283e76
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-08 11:40:37 -0700

    misc

--- Code/Steven/lab18_peaks_n_valleys4.py
+++ Code/Steven/lab18_peaks_n_valleys4.py
@@ -33,12 +33,13 @@ source_data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 max_val = max(source_data)
 # print('max val = ', max_val)
 
-for i in range(0, max_val, -1):
+for i in range(max_val, 0, -1):
     for j in range(len(source_data)):
         if source_data[j] >= i:
             print('X', end=' ')
         else:
             print(' ', end=' ')
+    print()
 
 
 
