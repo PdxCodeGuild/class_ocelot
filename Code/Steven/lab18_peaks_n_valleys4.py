fe81dad1ed475e9057230b576da40231cee25eab
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-08 11:37:28 -0700

    misc

--- Code/Steven/lab18_peaks_n_valleys4.py
+++ Code/Steven/lab18_peaks_n_valleys4.py
@@ -1,28 +1,28 @@
 
-
-def peaks_and_valleys(data):
-    peak_data = []
-    valley_data = []
-
-    for i in range(1, len(data) - 1):
-        data_current = data[i]
-        data_left = data[i - 1]
-        data_right = data[i + 1]
-
-        if data_left < data_current and data_right < data_current:
-            peak_data.append(i)
-
-        if data_left > data_current and data_right > data_current:
-            valley_data.append(i)
-
-    return peak_data, valley_data
+#
+# def peaks_and_valleys(data):
+#     peak_data = []
+#     valley_data = []
+#
+#     for i in range(1, len(data) - 1):
+#         data_current = data[i]
+#         data_left = data[i - 1]
+#         data_right = data[i + 1]
+#
+#         if data_left < data_current and data_right < data_current:
+#             peak_data.append(i)
+#
+#         if data_left > data_current and data_right > data_current:
+#             valley_data.append(i)
+#
+#     return peak_data, valley_data
 
 # print(f'Peaks: ' {peak_data}')
 # print(f'Valleys: ' {valley_data}')
 
 source_data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
-peak_data, valley_data = peaks_and_valleys(source_data)
+# peak_data, valley_data = peaks_and_valleys(source_data)
 
 # print(peak_data)
 # print(valley_data)
@@ -31,14 +31,15 @@ peak_data, valley_data = peaks_and_valleys(source_data)
 #     print('X' * source_data[i])
 
 max_val = max(source_data)
-print('max val = ', max_val)
+# print('max val = ', max_val)
 
 for i in range(0, max_val, -1):
-    print(i)
     for j in range(len(source_data)):
-        print(j)
-        if source_data[j] < i:
+        if source_data[j] >= i:
             print('X', end=' ')
+        else:
+            print(' ', end=' ')
+
 
 
 
