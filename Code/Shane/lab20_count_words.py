f14aa7e9bdee0c2f7fc896606aa2c8d27857fb84
Unitedideas <unitedideas@gmail.com>
2018-05-08 21:00:02 -0700

    Final version

--- Code/Shane/lab20_count_words.py
+++ Code/Shane/lab20_count_words.py
@@ -1,5 +1,21 @@
 import string
 
+
+def word_count(lines):
+    first_loop_count = 0
+    second_loop_count = 0
+    for word in lines:
+        if word not in words:
+            words.update({word: 1})
+            first_loop_count += 1
+        elif word in words:
+            words[word] = words[word]+1
+            second_loop_count += 1
+    print(f'{first_loop_count} - Original words')
+    print(f'{second_loop_count} - Duplicate words')
+    return words
+
+
 words = {}
 
 with open('the_battle_of_talavera.txt', 'r', encoding='utf-8') as f:
@@ -7,18 +23,12 @@ with open('the_battle_of_talavera.txt', 'r', encoding='utf-8') as f:
 
 for p in string.punctuation:
     contents = contents.replace(p, ' ')
-contents = contents.replace("\n", ' ')
 
 contents = contents.lower()
 
 lines = contents.split()
 
-#print(lines)
+top_ten = sorted((word_count(lines)).items(), key=lambda x: x[1], reverse = True)
 
-for word in lines:
-    if word not in words:
-        words.update({word: 1})
-        if word in words:
-            print('I\'m here')
-
-print(words)
+for i in range(min(10, len(top_ten))):
+    print(top_ten[i])
