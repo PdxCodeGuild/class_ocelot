b46c676a50bcd60644ff18bbe7cd3107532459b4
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-10 11:51:33 -0700

    ˆ

--- Code/Steven/lab21_ARI_3.py
+++ Code/Steven/lab21_ARI_3.py
@@ -10,16 +10,10 @@ wds_all = bk_contents.split()
 
 # list all words ending in a period.
 wds_period = []
-# wds_period_len = len(wds_all)
 for word in wds_all:
     char_last = word[-1]
     if char_last == '.':
-        # print(wds_all[int(j)])
-        # eos_str = wds_period[word - 1] + ' ' + wds_period[word]
-        # eos_str = wds_period[word - 1] + ' ' + wds_period[word]
-
         wds_period.append(word)
-
 
 # ...convert to dict.
 wds_period_d = {}
@@ -29,6 +23,8 @@ for wd_period in wds_period:
     else:
         wds_period_d[wd_period] += 1
 
+
+
 # wds_period_len
 
 # ...and convert back to list to sort by freq.
@@ -36,11 +32,8 @@ wds_per_freq = list(wds_period_d.items())
 wds_per_freq.sort(key=lambda tup: tup[1], reverse=True)
 
 
-
-# for i in range(int(len(wds_per_freq))):
-#     print(wds_per_freq[i])
-
-
+for i in range(len(wds_per_freq)):
+    print(wds_per_freq[i])
 
 
 
---
+++ Code/Steven/temp01.py
@@ -0,0 +1,5 @@
+test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in range(len(test_list)):
+    print(test_list[i - 1], test_list[i])
+
