7da7e6f2f0374903b71dcb2075b8aa6c9d501316
Shane Cheek <unitedideas@gmail.com>
2018-05-10 10:56:02 -0700

    Checking and updating old programs

--- Code/Shane/lab_21_compute_automated_readability_index.py
+++ Code/Shane/lab_21_compute_automated_readability_index.py
@@ -1,11 +1,31 @@
+import re
 
-
+ari_scale = {
+     1: {'ages':   '5-6', 'grade_level': 'Kindergarten'},
+     2: {'ages':   '6-7', 'grade_level':    '1st Grade'},
+     3: {'ages':   '7-8', 'grade_level':    '2nd Grade'},
+     4: {'ages':   '8-9', 'grade_level':    '3rd Grade'},
+     5: {'ages':  '9-10', 'grade_level':    '4th Grade'},
+     6: {'ages': '10-11', 'grade_level':    '5th Grade'},
+     7: {'ages': '11-12', 'grade_level':    '6th Grade'},
+     8: {'ages': '12-13', 'grade_level':    '7th Grade'},
+     9: {'ages': '13-14', 'grade_level':    '8th Grade'},
+    10: {'ages': '14-15', 'grade_level':    '9th Grade'},
+    11: {'ages': '15-16', 'grade_level':   '10th Grade'},
+    12: {'ages': '16-17', 'grade_level':   '11th Grade'},
+    13: {'ages': '17-18', 'grade_level':   '12th Grade'},
+    14: {'ages': '18-22', 'grade_level':      'College'}
+}
 
 with open('the_battle_of_talavera.txt','r', encoding = 'utf-8') as f:
     contents = f.read()
 
-print(contents)
+words = len(re.findall(r'[\w]+', contents))
 
+chars = len(re.findall(r'[\w]', contents))
 
+sentences = len(re.findall(r'[^!.?]+', contents))
 
+score = round((4.71*(chars/words)+.5*(words/sentences)-21.43)+.5)
 
+print(f'ARI score of {score} = {ari_scale[score]}')
