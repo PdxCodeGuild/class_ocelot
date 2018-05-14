11fca9e5fa5629b328dd454b4fef9c73061f4a84
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-14 15:25:45 -0700

    Rain data for Crime Data work

--- Code/Nathan/lab24_Crime-Data-2.py
+++ Code/Nathan/lab24_Crime-Data-2.py
@@ -1,16 +1,5 @@
-1
 
-
-data = []
+n_lines = 100
 with open('../../1 Python/data/Open_Data_Sheet_data.csv', 'r') as f:
-    contents = f.read()
-    lines = contents.split('\n')
-    lines.pop(0) # remove header
-    # for line in lines:
-    for line in range(100):
-        if line == '':
-            continue
-        line = line.split(',')
-        data.append([float(v) if v != '' else -1 for v in line])
-
-print(data)
+    lines = [next(f) for _ in range(n_lines)]
+print('\n'.join(lines))
