df92fa98c35699b0ff509b5287e43a138f256026
Nathan Kinney <nathankeithkinney@gmail.com>
2018-05-15 09:41:17 -0700

    v1 got help

--- Code/Nathan/lab24_Crime-Data-2.py
+++ Code/Nathan/lab24_Crime-Data-2.py
@@ -6,7 +6,7 @@ from itertools import groupby
 def parse_csv(path):
     with open(path, 'r', encoding='utf8') as f:
         data_points = list()
-        lines = f.readlines()[:1000000]
+        lines = f.readlines()
         keys = [key.casefold().strip().replace(' ', '_').replace('\ufeff', '') for key in lines[0].split(',')]
 
         for line in lines[1:]:
@@ -20,15 +20,21 @@ data_points = parse_csv('../../1 Python/data/Open_Data_Sheet_data.csv')
 
 
 crimes = [data for data in data_points if data['crime_against'] == 'property']
-crimes = sorted(crimes, key=lambda c: c['neighborhood'])
+#crimes = [data for data in data_points if data['crime_against'] == 'property']
+
+def sort_by_neighborhood(c):
+    return c['neighborhood']
+
+crimes = sorted(crimes, key=lambda c: c['offense_type'])
 
 print(len(crimes))
-
-
 
 crimes_by_neighborhood = dict()
 for neighborhood, crimes in groupby(crimes, key=lambda c: c['neighborhood']):
     crimes_by_neighborhood[neighborhood] = list(crimes)
+#crimes_by_type = dict()
+#for offense_type, crimes in groupby(crimes, key=lambda c: c['offense_type']):
+#    crimes_by_type['offense_type'] = list(crimes)
 
-most_crimes = max(crimes_by_neighborhood.items(), key=lambda x: len(x[1]))
+most_crimes = max(crimes_by_type.items(), key=lambda x: len(x[1]))
 print(most_crimes[0], len(most_crimes[1]))
