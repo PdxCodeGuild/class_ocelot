4fe5de6b43701020172a06b57cd6174ae994d521
Nathan Kinney <nathankeithkinney@gmail.com>
2018-05-16 09:51:16 -0700

    v1 hope to god it works

---
+++ Code/Nathan/Lab24Crime_Data.py
@@ -0,0 +1,51 @@
+#dont touch actually works
+
+from itertools import groupby
+
+
+def parse_csv(path):
+    with open(path, 'r', encoding='utf8') as f:
+        data_points = list()
+        lines = f.readlines()
+        keys = [key.casefold().strip().replace(' ', '_').replace('\ufeff', '') for key in lines[0].split(',')]
+
+        for line in lines[1:]:
+            values = [value.casefold().strip() for value in line.split(',')]
+            data_points.append(dict(zip(keys, values)))
+
+        return data_points
+
+
+data_points = parse_csv('../../1 Python/data/Open_Data_Sheet_data.csv')
+
+
+crimes = [data for data in data_points if data['offense_type'] == 'burglary']
+
+#crimes = [data for data in data_points if data['crime_against'] == 'property']
+
+#def sort_by_neighborhood(c):
+#    return c['neighborhood']
+
+crimes = sorted(crimes, key=lambda c: c['offense_type'])
+
+print(len(crimes))
+
+# print(len(list(groupby(crimes, key=lambda c: c['neighborhood']))))
+
+crimes_by_type = dict()
+for offense_type, crimes in groupby(crimes, key=lambda c: c['offense_type']):
+    crimes_by_type[offense_type] = list(crimes)
+
+
+#crimes_by_type = dict()
+#for offense_type, crimes in groupby(crimes, key=lambda c: c['offense_type']):
+#    crimes_by_type['offense_type'] = list(crimes)
+
+# print(crimes_by_neighborhood.items())
+
+# crimes_by_neighborhood = [len(x[1]) for x in crimes_by_neighborhood.items()]
+#
+# print(crimes_by_neighborhood)
+
+most_crimes = max(crimes_by_type.items(), key=lambda x: len(x[1]))
+print(most_crimes[0], len(most_crimes[1]))
