96d507317c3381c139acc2ee5103cf93847d19f3
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-05-27 20:28:12 -0700

    ˆ

---
+++ Code/Steven/_CURRENT ACTIVE/Homework Analysis 01/Count all md 01.py
@@ -0,0 +1,8 @@
+import glob
+import os
+import shutil
+
+
+for filename in glob.iglob('Code/Steven/_CURRENT ACTIVE/Homework Analysis 01/class_ocelot_all_markdown/PCG 1 docs/', recursive=True):
+    print(filename)
+
