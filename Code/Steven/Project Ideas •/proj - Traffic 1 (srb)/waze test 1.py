8b22d4da6f653331798e3ed9d575fb50a512eb7a
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-06-04 10:23:39 -0700

    organizing

--- Code/Steven/Project Ideas •/proj - Traffic 1 (srb)/waze test 1.py
+++
@@ -1,7 +0,0 @@
-import WazeRouteCalculator
-
-from_address = '608 Northwest 70th Circle, Vancouver, WA'
-to_address = 'Southwest Corbett Avenue, Portland, OR'
-region = 'US'
-route = WazeRouteCalculator.WazeRouteCalculator(from_address, to_address, region)
-route.calc_route_info()
