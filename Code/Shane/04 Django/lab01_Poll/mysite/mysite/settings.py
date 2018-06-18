272cd6950bd5cf0f2423696a93a9c49a806a54d8
Unitedideas <unitedideas@gmail.com>
2018-06-18 14:26:29 -0700

    starting the poll project
    Signed-off-by: Unitedideas <unitedideas@gmail.com>

--- Code/Shane/04 Django/lab01_Poll/mysite/mysite/settings.py
+++ Code/Shane/04 Django/lab01_Poll/mysite/mysite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
 ]
 
 MIDDLEWARE = [
@@ -107,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Los_Angeles'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
