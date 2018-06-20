ecfd11dd93dd81c94cba764bbb44cd6d78f42f7f
Shane Cheek <unitedideas@gmail.com>
2018-06-20 15:25:10 -0700

    starting the todo app
    Signed-off-by: Shane Cheek <unitedideas@gmail.com>

--- Code/Shane/04 Django/lab01.5_Poll/mysite/polls/views.py
+++ Code/Shane/04 Django/lab01.5_Poll/mysite/polls/views.py
@@ -26,6 +26,8 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question': question})
 
 
+
+#submit form
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
--- Code/Shane/04 Django/lab01.5_Poll/mysite/todo/views.py
+++ Code/Shane/04 Django/lab01.5_Poll/mysite/todo/views.py
@@ -6,8 +6,14 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    context = {
-        'HI THERE'
-    }
-    return HttpResponse(context)
+        #if request == POST:
 
+    # get all the todo items out of the database
+    # pass those as part of the context to render the template
+    return render(request, 'todo/index.html', {'name':'joanne'})
+    # go to the bd and get the display      ... ... ...objects.all()
+
+def add_todo(request):
+    # save data from request.POST in database
+    # redirect back to the index page (HttpResponseRedirect)
+    return HttpResponse('add todo!!!')
