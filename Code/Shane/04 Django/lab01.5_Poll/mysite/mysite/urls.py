e39940bd4d3f2ebbca2596508e77106d7618c9df
Shane Cheek <unitedideas@gmail.com>
2018-06-20 13:50:10 -0700

    starting the todo app
    Signed-off-by: Shane Cheek <unitedideas@gmail.com>

--- Code/Shane/04 Django/lab01.5_Poll/mysite/mysite/urls.py
+++ Code/Shane/04 Django/lab01.5_Poll/mysite/mysite/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('todo/', include('todo.urls')),
+
 ]
--- Code/Shane/04 Django/lab01.5_Poll/mysite/todo/views.py
+++ Code/Shane/04 Django/lab01.5_Poll/mysite/todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    context = {
+        'HI THERE'
+    }
+    return HttpResponse(context)
+
