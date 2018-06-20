ea84f37ecff56e900a72c20c055b25a69502fe2d
Nathan Kinney <nathankeithkinney@gmail.com>
2018-06-20 15:40:38 -0700

    v2 working add button

--- Code/Nathan/Django/mysite/todo/urls.py
+++ Code/Nathan/Django/mysite/todo/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+from django.urls import path
+
+from . import views
+
+app_name = 'todo'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_todo/', views.add_todo, name='add_todo')
 ]
