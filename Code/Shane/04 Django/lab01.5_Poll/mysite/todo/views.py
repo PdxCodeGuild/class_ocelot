0b04e62de9e8be3a4f79d54d41c951c3be78e1bb
Unitedideas <unitedideas@gmail.com>
2018-06-20 21:47:56 -0700

    starting the todo app
    Signed-off-by: Unitedideas <unitedideas@gmail.com>

--- Code/Shane/04 Django/lab01.5_Poll/mysite/todo/views.py
+++ Code/Shane/04 Django/lab01.5_Poll/mysite/todo/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-
+from .models import TodoItem
 from django.template import loader
+from django.urls import reverse
 
 # Create your views here.
 
 def index(request):
-        #if request == POST:
+    somevar = TodoItem.objects.all()
 
-    # get all the todo items out of the database
-    # pass those as part of the context to render the template
-    return render(request, 'todo/index.html', {'name':'joanne'})
+    context = {
+        'todo_items123': somevar
+            # somevar[0].todo_text #<-- This is from models column 1 named todo_text
+    }
+
+    return render(request, 'todo/index.html', context)
     # go to the bd and get the display      ... ... ...objects.all()
 
 def add_todo(request):
+    todo_text = request.POST['todo_item']
+    todo_item = TodoItem(todo_text=todo_text, completed=False)
+    todo_item.save()
+    # item_on_list = question.choice_set.get(pk=request.POST['choice'])
     # save data from request.POST in database
     # redirect back to the index page (HttpResponseRedirect)
-    return HttpResponse('add todo!!!')
+
+    return HttpResponseRedirect(reverse('todo:index'))
