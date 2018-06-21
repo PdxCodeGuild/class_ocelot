1ff8339f5e92a4e9051eaa8cf35b54a49151355d
Shane Cheek <unitedideas@gmail.com>
2018-06-21 11:10:35 -0700

    ˚
    Signed-off-by: Shane Cheek <unitedideas@gmail.com>

--- Code/Shane/04_Django/lab01.5_Poll/mysite/todo/urls.py
+++ Code/Shane/04_Django/lab01.5_Poll/mysite/todo/urls.py
@@ -7,7 +7,9 @@ app_name = 'todo'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
-    path('add_todo/', views.add_todo, name='add_todo')
+    path('add_todo/', views.add_todo, name='add_todo'),
+    path('removed_item/', views.remove_todo, name='todo_remove_unicorn'),
+
     # ex: /polls/5/
     ### path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
