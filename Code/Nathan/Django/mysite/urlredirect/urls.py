e3fea405d5e430019c495d40f13a7ad69ecf104c
Nathan Kinney <nathankeithkinney@gmail.com>
2018-06-22 15:10:32 -0700

    v1 got th url input working

--- Code/Nathan/Django/mysite/urlredirect/urls.py
+++ Code/Nathan/Django/mysite/urlredirect/urls.py
@@ -11,4 +11,6 @@ app_name = 'urlredirect'
 urlpatterns = [
     path('', views.index, name='index'),
     path('save_shortener/', views.save_shortener, name='save_shortener'),
+    path('redirect_form/', views.redirect_form, name='redirect_form'),
+    path('<str:code>/', views.short_url_redirect, name='short_url_redirect'),
 ]
--- Code/Nathan/Django/mysite/urlredirect/views.py
+++ Code/Nathan/Django/mysite/urlredirect/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.utils import timezone
 import random
@@ -9,30 +9,33 @@ import string
 from .models import Shortener
 
 def index(request):
-
-    return render(request, 'urlredirect/index.html')
-
+    context = {'shorteners': Shortener.objects.all()}
+    return render(request, 'urlredirect/index.html', context)
 
 def get_random_code():
+
     return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(6)])
 
-
-# def get_random_unique_code():
-#     code = get_random_code()
-#     while Shortener.objects.get(code=code) is not None:
-#         code = get_random_code()
-#     return code
-
-
 def save_shortener(request):
+
     full_url = request.POST['furl']
     code = get_random_code()
     shortener = Shortener(code=code, urls=full_url)
     shortener.save()
 
     return HttpResponseRedirect(reverse('urlredirect:index'))
+    # return HttpResponseRedirect('urlredirect:index')
 
 
-def random_code(request):
+def short_url_redirect(request, code):
+    # find the Shortener with this code
+    shortener = Shortener.objects.get(code=code)
 
-    return redirect(object)
+    return redirect(shortener.urls)
+
+def redirect_form(request):
+    code = request.POST['code']
+    shortener = Shortener.objects.get(code=code)
+
+    return redirect(shortener.urls)
+
