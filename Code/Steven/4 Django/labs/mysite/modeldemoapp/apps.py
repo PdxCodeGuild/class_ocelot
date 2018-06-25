8d65baca079c525045b48d0cabf1110a499b9232
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-06-25 12:29:52 -0700

    set up django app quizmaker

--- Code/Steven/4 Django/labs/mysite/modeldemoapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ModeldemoappConfig(AppConfig):
-    name = 'modeldemoapp'
--- Code/Steven/4 Django/labs/thesite/thesite/settings.py
+++ Code/Steven/4 Django/labs/thesite/thesite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'publiclibrary.apps.PubliclibraryConfig'
     'theshortening.apps.TheshorteningConfig',
     'django.contrib.admin',
     'django.contrib.auth',
