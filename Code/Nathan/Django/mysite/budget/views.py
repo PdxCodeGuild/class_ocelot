61907e7ef2d5e8bfd350d61a520b39f7b5365564
Nathan Kinney <nathankeithkinney@gmail.com>
2018-06-25 14:20:11 -0700

    v1

---
+++ Code/Nathan/Django/mysite/budget/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def project_list(request):
+    return render(request, 'budget/project_list.html')
+
+def project_detail(request, project_slug):
+    return render(request, 'budget/project_detail.html')
