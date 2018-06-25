c3397c4ac3e8e5a16ce3369859e69656bc50a45b
Steven Brown <1176501+manufactured@users.noreply.github.com>
2018-06-25 15:16:29 -0700

    set up django app quizmaker

--- Code/Steven/4 Django/labs/thesite/publiclibrary/models.py
+++ Code/Steven/4 Django/labs/thesite/publiclibrary/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+# from django.contrib.auth.models import Author
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField( max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField( max_length=30)
+    publish_date = models.DateTimeField()
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    #
+    # def author(self):
+    #     return self.book.author
+
+
+
+
